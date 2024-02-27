cbb5b8d3c66dba2ac952fb371c20dc1ebae5280b
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-02-27 10:08:13 -0800

    [ADDED]:shubham This program for Addition in command line

---
+++ CMDArgument.java
@@ -0,0 +1,17 @@
+
+class CMDArgument
+{
+	public static void main(String args[])
+	{
+		int a;
+		int b;
+		int c;
+
+		a = Integer.parseInt(args[0]);
+		b =Integer.parseInt(args[1]);
+		
+	  	c = a + b;
+		System.out.println("Addition="+c);
+			
+	}
+}
