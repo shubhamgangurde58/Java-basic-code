9f1d530380695b334ee7b1834b9a16ba6058c8dd
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-15 20:20:40 +0530

    [ADDED]:shubham This program of String class

---
+++ StringDemo.java
@@ -0,0 +1,19 @@
+class StringDemo{
+
+	public static void main(String args[])
+	{
+	    String name1 = "Ritesh";
+	    String name2 = new String("Shubham");
+	    name2 = "jayesh";
+	    System.out.println("Yogesh");
+
+	    String name3 = "Ritesh";
+	     
+
+	System.out.println(name1);
+	System.out.println(name2);
+	System.out.println(name2);
+	System.out.println(name3);
+		
+	}
+}
