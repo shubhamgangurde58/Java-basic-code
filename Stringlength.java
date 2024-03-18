e49ec52a42363d63f1bf12595cecc83233e31ddf
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-18 20:14:08 +0530

    [ADDED]:shubham This code for String length()

---
+++ Stringlength.java
@@ -0,0 +1,12 @@
+class Stringlength{
+
+	public static void main(String[] args){
+	    
+	    int c;
+	    String name = "Shubham";
+	    c = name.length();   
+	   
+	    System.out.println("Length = "+c);
+	    
+	}
+}
