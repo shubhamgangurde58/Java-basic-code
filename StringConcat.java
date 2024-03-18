a74795621fd1e18a6273d2138c2e9d1329f9ebb5
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-18 20:18:50 +0530

    [ADDED]:shubham This code for String Concat

---
+++ StringConcat.java
@@ -0,0 +1,10 @@
+class StringConcat{
+
+	public static void main(String[] args){
+
+		String name = new String("Shubham");
+		String lname = new String("Gangurde");
+		
+		System.out.println(name.concat(lname));
+	}
+}
