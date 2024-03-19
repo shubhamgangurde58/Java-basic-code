c7e3d38b8b6e777b3f90c61c2ba24d1d0c2832f7
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-19 20:03:24 +0530

    [ADDED]:shubham This code for CharAt method

---
+++ String_charAt.java
@@ -0,0 +1,11 @@
+class String_charAt{
+
+	public static void main(String[] args){
+
+		String name = "Shubham";
+
+		System.out.println(name.charAt(1));
+		System.out.println(name.charAt(3));
+	}
+
+}
