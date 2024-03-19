17cb9c8bf945c31246233a55f51ea291b5549d3e
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-19 19:23:36 +0530

    [ADDED]:shubham This code for startsWith() and endsWith()

---
+++ StringStartwith.java
@@ -0,0 +1,12 @@
+class StringStartwith{
+
+	public static void main(String[] args){
+	
+		String name = "shubham gangurde";
+		System.out.println(name.startsWith("s"));
+		System.out.println(name.endsWith("m"));
+
+		System.out.println(name.startsWith("g"));
+		System.out.println(name.endsWith("e"));
+	}
+}
