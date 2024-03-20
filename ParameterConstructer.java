fcb2689250806783b752b7faff78e8a4de900b78
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-20 22:21:57 +0530

    [ADDED]:shubham This program for parameter constructer

---
+++ ParameterConstructer.java
@@ -0,0 +1,17 @@
+
+class ParameterConstructer{
+
+	ParameterConstructer(int a,int b){
+	int c;
+	c=a+b;
+	System.out.println("Addition="+c);
+	}
+	
+	public static void main(String[] args){
+	
+		int a;
+		int b;
+	ParameterConstructer constructer = new ParameterConstructer(10 ,20);
+
+	}
+}
