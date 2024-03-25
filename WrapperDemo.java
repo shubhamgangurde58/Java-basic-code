7e72f9cb21849bb191e339dbe894d4a3d36c2213
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-25 21:51:32 +0530

    [ADDED]:shubham This Program for Wrapper Class Demo

---
+++ WrapperDemo.java
@@ -0,0 +1,15 @@
+class WrapperDemo{
+
+	public static void main(String[] args){
+		
+		int a = 20;
+	
+		Integer i =Integer.valueOf(a);
+		Integer j = a;
+
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(j);
+			
+	}
+}
