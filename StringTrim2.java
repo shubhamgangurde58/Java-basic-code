b4d60bcbb4bc45fe5b35ef6424ac10a0f66e34f1
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-25 22:12:05 +0530

    [ADDED]:shubham This code for String Trim2 Demo

---
+++ StringTrim2.java
@@ -0,0 +1,10 @@
+class StringTrim2{
+
+	public static void main(String[] args){
+		
+		String name = "  shubham Gangurde  ";
+		String name2 = "  Welcome  ";
+		System.out.println(name.trim());
+		System.out.println(name2.trim());
+	}
+}
