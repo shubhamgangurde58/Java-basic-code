2607762b5e5aeced971ebf688f76e738638edb1e
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-09 17:21:49 +0530

    [ADDED]:shubham This code do while loop demo

---
+++ do_whileloopDemo.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;;
+class do_whileloopDemo{
+
+	public static void main(String args[])
+	{
+		int n;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a no:");
+		n = scanner.nextInt();
+		int i=0;
+		do
+		{
+			System.out.println("Dennic Richies");
+			i++;
+	
+		}while(i<n);
+	}
+}
