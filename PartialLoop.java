be8252524ce2d59e0ba7aa47004f3141d5093ffc
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-15 19:46:32 +0530

    [ADDED]:shubham This code for Partial loop Demo

---
+++ PartialLoop.java
@@ -0,0 +1,14 @@
+class PartialLoop{
+
+	public static void main(String arg[])
+	{
+		int n=10;
+		int i;
+		int j;
+		
+		for(i=0,j=10;i<n;i++,j--)
+		{
+			System.out.println(i+"\t"+j);
+		}
+	}
+}
