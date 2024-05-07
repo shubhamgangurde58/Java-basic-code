7ff609c5a1253523e1433e38600f59043402e4da
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-05-07 22:58:17 +0530

    [ADDED]: shubham This code for String Function equals

---
+++ String_equal2.java
@@ -0,0 +1,14 @@
+class String_equal2{
+
+	public static void main(String[] args){
+		
+		String a = "Welcome";
+		String b = "Welcome";
+
+		String c = "Shubham";
+		
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
+		System.out.println(a.equals(c));
+	}
+}
