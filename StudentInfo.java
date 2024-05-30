6070b0ce40364cbb4adfb150dde3bdb453ec32e5
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-05-30 23:11:19 +0530

    [ADDED]: shubham this code for Student information

---
+++ StudentInfo.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+class StudentInfo
+{
+		Scanner scanner = new Scanner(System.in);
+		int rollno;
+		String name;
+		Double fees;
+
+		public void inputfun()
+		{	
+			System.out.println("Enter the rollno:");
+			rollno = scanner.nextInt();
+
+			System.out.println("Enter the name:");
+			name = scanner.next();
+		
+			System.out.println("Enter the Fees:");
+			fees = scanner.nextDouble();
+	
+		}
+		public void outputfun()
+		{
+			System.out.println("rollno:"+rollno);
+			System.out.println("name:"+name);
+			System.out.println("fees:"+fees);
+		}
+
+	public static void main(String args[])
+	{
+		StudentInfo studentdemo = new StudentInfo();
+		studentdemo.inputfun();
+		studentdemo.outputfun();
+	}
+}
