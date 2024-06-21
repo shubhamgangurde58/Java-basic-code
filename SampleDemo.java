e1d65f09ab840f3b60737efe8286effde6c86dc0
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-06-21 22:53:03 +0530

    [ADDED]: shubham

---
+++ SampleDemo.java
@@ -0,0 +1,19 @@
+class Sample{
+	
+	int a;
+	int b;
+	 
+ 	Sample(){
+
+		a=b=c=0;
+		System.out.println("A="+a);
+		System.out.println("B="+b);
+		System.out.println("c="+c);
+	}
+
+	public static void main(String args[])
+	{
+		Sample sample = new Sample();
+		
+	}
+}
