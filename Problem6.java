b97cd7b8bd5d9c8e4a5bde6f44552bef60eeae64
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-07-26 21:32:27 +0530

    [ADDED]: shubham This code for Problem 6

---
+++ Problem6.java
@@ -0,0 +1,15 @@
+class Problem6{
+
+	public static void main(String args[]){
+	
+		System.out.println("---Write a infinite loop using do while condition---");
+	
+		do{
+
+			System.out.print("Hello java");
+
+		}while(true);
+
+		
+	}
+}
