c26113ec00579d234abc61bc0dd343ebb1487a0c
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-07-28 21:13:22 +0530

    [ADDED]: shubham this code for print factorial with ricursion function

---
+++ RecursionQuestion1.java
@@ -0,0 +1,23 @@
+class RecursionQuestion1{
+
+	public static void printNumber(int n){
+
+		if(n == 0){
+
+			return ;
+		}
+
+		System.out.println(n);
+
+		printNumber(n-1);
+		
+	}
+
+	public static void main(String args[]){
+
+		int n = 5;
+
+		printNumber(n);
+
+	}
+}
