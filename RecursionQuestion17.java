d030cbe7d9257b05fec005588a9e5172f04e8a24
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-08-01 22:09:22 +0530

    [ADDED]: shubham This code for Recursion 17

---
+++ RecursionQuestion17.java
@@ -0,0 +1,34 @@
+
+class RecursionQuestion17{
+	
+	public static String[] keypad = {".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+	public static void printCom(String str , int index , String combination){
+
+		if(index == str.length()){
+	
+			System.out.println(combination);
+			return;
+		}
+
+		char currChar = str.charAt(index);
+
+		String mapping = keypad[currChar - '0'];
+		
+		for(int i=0;i<mapping.length();i++){
+			
+			printCom(str,index+1,combination+mapping.charAt(i));
+			
+		}
+	
+	}
+
+	public static void main(String args[]){
+
+		String str="451";
+
+		printCom(str,0,"");
+	
+
+	}
+}
