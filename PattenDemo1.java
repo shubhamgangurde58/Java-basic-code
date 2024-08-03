e5dc7767ad6de1b3dd259d6f0776aadbd8b24750
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-08-03 18:59:14 +0530

    [ADDED]: shubham This code for solid rectangle

---
+++ PattenDemo1.java
@@ -0,0 +1,17 @@
+class PattenDemo1{
+
+	public static void main(String args[]){
+	
+		//System.out.println("***Solid Rectangle***\n\n");
+
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+}
