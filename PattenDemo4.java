45672c3db94709d9772cc264ef2407d23c8d2dfc
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-08-04 13:08:40 +0530

    [ADDED]: shubham This code for Inverted half pyramid

---
+++ PattenDemo4.java
@@ -0,0 +1,24 @@
+class PattenDemo4{
+
+	public static void main(String args[]){
+
+		System.out.println("----Inverted Half Pyramid----");
+
+		for(int i=0;i<=5;i++){
+
+			for(int j=0;j<=5;j++){
+
+				if(i>j)
+				{
+					
+				}else{
+
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+		
+}
