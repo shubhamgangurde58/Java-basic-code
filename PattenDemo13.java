0bee1f015e7cc78f8b32a2ec76b5e92ec107b2d6
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-08-07 18:36:36 +0530

    [ADDED]: shubham This code for Rectangle Print

---
+++ PattenDemo13.java
@@ -0,0 +1,28 @@
+class PattenDemo13{
+
+	public static void main(String args[]){
+		
+		int n=5;
+		
+		for(int i=1;i<=n;i++){
+
+			int m=n;
+			for(int j=1;j<=n-i;j++){
+			
+				System.out.print(" ");
+			}
+
+			for(int j=i;j>=1;j--){
+				
+				System.out.print(j);
+			}
+
+			for(int j=2;j<=i;j++){
+
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+}
