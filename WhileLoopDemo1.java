e0fb395434a557637a705f2723f2112adb4bbb2b
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-10-14 19:53:53 +0530

    [ADDED] shubham

---
+++ WhileLoopDemo1.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+class WhileLoopDemo1{
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no:");
+		int n=sc.nextInt();
+		int i=0;
+		while(i<n){
+
+			System.out.println("cpp Develop by Bjarne Stroustrup");
+			i++;
+		}
+	}
+}
