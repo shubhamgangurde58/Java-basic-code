6c5c4834bf84fe93153c71011e2ec65a886dac23
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-10-17 21:55:24 +0530

    [ADDED]: shubham This code for CMDArgument Demno

---
+++ CMDArgumentDemo1.java
@@ -0,0 +1,16 @@
+class CMDArgumentDemo1{
+
+	public static void main(String args[]){
+	
+		int a;
+		int b;
+
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+	
+		int c = a+b;
+
+		System.out.println("Additon="+c);
+
+	}
+}
