f4c9ec5185e092e78a710e7a29721aec8644d4c7
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-08-26 09:37:29 +0530

    [ADDED]: shubham This code for Arithmatic Operator Demo

---
+++ ArithmaticOperator.java
@@ -0,0 +1,26 @@
+class ArithmaticOperator{
+
+    public static void main(String args[]){
+
+        int a = 2;
+        int b = 20;
+
+        // +, -, *, \, %;
+        int c = a+b;
+        System.out.println(c);
+
+        int d = a-b;
+        System.out.println(d);
+
+        int e = a*b;
+        System.out.println(e);
+
+        int f = a/b;
+        System.out.println(f);
+
+        int g = a%b;
+        System.out.println(g);
+
+    }
+
+}
