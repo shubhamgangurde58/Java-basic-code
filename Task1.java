6c1c12f413ddf7412fbfa86db331443b1747c483
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-11-06 09:18:54 +0530

    [ADDED]: shubham This code for Task 1 Demo

---
+++ Task1.java
@@ -0,0 +1,18 @@
+class Task1{
+
+	public Task1(){
+
+		String name = "Chetan";
+		int age = 21;
+		String city = "shahada";
+
+		System.out.println("Name :"+name+"\n Age :"+age+"\n City :"+city);
+	
+	}
+
+	public static void main(String args[]){
+
+		new Task1();
+	
+	}
+}
